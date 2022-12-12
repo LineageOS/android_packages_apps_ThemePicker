bace710826a15e257d024d8320ea9cd644d07ff2
Ido Ben-Hur <idoybh2@gmail.com>
2022-12-12 15:50:42 +0200

    ThemePicker: Fixup font and icon picking wrong user ID
    Logs:
    E AndroidRuntime: java.lang.SecurityException: Permission Denial: get/set setting for user asks to run as user -2 but is calling from uid u0a164; this requires android.permission.INTERACT_ACROSS_USERS_FULL
    E AndroidRuntime:        at android.os.Parcel.createExceptionOrNull(Parcel.java:3021)
    E AndroidRuntime:        at android.os.Parcel.createException(Parcel.java:3005)
    E AndroidRuntime:        at android.os.Parcel.readException(Parcel.java:2981)
    E AndroidRuntime:        at android.database.DatabaseUtils.readExceptionFromParcel(DatabaseUtils.java:190)
    E AndroidRuntime:        at android.database.DatabaseUtils.readExceptionFromParcel(DatabaseUtils.java:142)
    E AndroidRuntime:        at android.content.ContentProviderProxy.call(ContentProviderNative.java:732)
    E AndroidRuntime:        at android.provider.Settings.getStringForUser(Settings.java:3202)
    E AndroidRuntime:        at android.provider.Settings.getStringForUser(Settings.java:6814)
    E AndroidRuntime:        at com.android.customization.model.font.FontManager.persistOverlay(FontManager.java:112)
    E AndroidRuntime:        at com.android.customization.model.font.FontManager.apply(FontManager.java:63)
    E AndroidRuntime:        at com.android.customization.picker.font.FontFragment.applyFontOption(FontFragment.java:134)
    E AndroidRuntime:        at com.android.customization.picker.font.FontFragment.lambda-android-customization-picker-font-FontFragment(FontFragment.java:129)
    E AndroidRuntime:        at com.android.customization.picker.font.FontFragment3405756ExternalSyntheticLambda1.onClick(Unknown Source:2)
    E AndroidRuntime:        at com.android.wallpaper.widget.BottomActionBar.lambda-android-wallpaper-widget-BottomActionBar(BottomActionBar.java:346)
    E AndroidRuntime:        at com.android.wallpaper.widget.BottomActionBar3405756ExternalSyntheticLambda3.onClick(Unknown Source:6)
    E AndroidRuntime:        at android.view.View.performClick(View.java:7506)
    E AndroidRuntime:        at android.view.View.performClickInternal(View.java:7483)
    E AndroidRuntime:        at android.view.View.-3405756Nest(Unknown Source:0)
    E AndroidRuntime:        at android.view.View.run(View.java:29342)
    E AndroidRuntime:        at android.os.Handler.handleCallback(Handler.java:942)
    E AndroidRuntime:        at android.os.Handler.dispatchMessage(Handler.java:99)
    E AndroidRuntime:        at android.os.Looper.loopOnce(Looper.java:201)
    E AndroidRuntime:        at android.os.Looper.loop(Looper.java:288)
    E AndroidRuntime:        at android.app.ActivityThread.main(ActivityThread.java:7879)
    E AndroidRuntime:        at java.lang.reflect.Method.invoke(Native Method)
    E AndroidRuntime:        at com.android.internal.os.RuntimeInit.run(RuntimeInit.java:550)
    E AndroidRuntime:        at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:936)
    E AndroidRuntime: Caused by: android.os.RemoteException: Remote stack trace:
    E AndroidRuntime:        at com.android.server.am.UserController.handleIncomingUser(UserController.java:2270)
    E AndroidRuntime:        at com.android.server.am.ActivityManagerService.handleIncomingUser(ActivityManagerService.java:12564)
    E AndroidRuntime:        at android.app.ActivityManager.handleIncomingUser(ActivityManager.java:4297)
    E AndroidRuntime:        at com.android.providers.settings.SettingsProvider.resolveCallingUserIdEnforcingPermissionsLocked(SettingsProvider.java:2298)
    E AndroidRuntime:        at com.android.providers.settings.SettingsProvider.getSecureSetting(SettingsProvider.java:1570)
    
    Change-Id: Iaff6e2e72ba24d81b8dbf16cca660b279455ff74

--- src/com/android/customization/model/iconpack/IconPackManager.java
+++ src/com/android/customization/model/iconpack/IconPackManager.java
@@ -98,7 +98,7 @@ public class IconPackManager implements CustomizationManager<IconPackOption> {
 
     private boolean persistOverlay(IconPackOption toPersist) {
         String value = Settings.Secure.getStringForUser(mContext.getContentResolver(),
-                Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES, UserHandle.USER_CURRENT);
+                Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES, UserHandle.myUserId());
         JSONObject json;
         if (value == null) {
             json = new JSONObject();
@@ -126,7 +126,7 @@ public class IconPackManager implements CustomizationManager<IconPackOption> {
         // updating the setting
         Settings.Secure.putStringForUser(mContext.getContentResolver(),
                 Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES,
-                json.toString(), UserHandle.USER_CURRENT);
+                json.toString(), UserHandle.myUserId());
         return true;
     }
 
