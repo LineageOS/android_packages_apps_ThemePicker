9c9fc0dc1b361e325353f605fa716103363e0763
Austin Wang <wangaustin@google.com>
2023-05-18 15:56:55 -0700

    Make Clock options resettable (1/3)
    Moved `ClockPickerSnapshotRestorer` to `ThemePicker`.
    
    `ClockPickerSnapshotRestorer` creates snapshots based on `ClockOptionModel` which contains all clock related options. The original snapshot created from the latest options in the repository, after user made a change, the snapshot is created from the repository and override with the change, this is to prevent race condition where the change has not been written in the `SecureSettings`.
    
    Bug: 274403822
    Test: play with Clock options
    Change-Id: Ia5475e3b367604b96a3dc6533c4cdbacc1f1fcb5

--- src/com/android/customization/picker/clock/data/repository/ClockPickerRepository.kt
+++ src/com/android/customization/picker/clock/data/repository/ClockPickerRepository.kt
@@ -42,7 +42,7 @@ interface ClockPickerRepository {
      * @param colorToneProgress color tone from 0 to 100 to apply to the selected color
      * @param seedColor the actual clock color after blending the selected color and color tone
      */
-    fun setClockColor(
+    suspend fun setClockColor(
         selectedColorId: String?,
         @IntRange(from = 0, to = 100) colorToneProgress: Int,
         @ColorInt seedColor: Int?,
