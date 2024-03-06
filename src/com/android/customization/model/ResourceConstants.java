e5aecd5c48d29b2cf79a63af160472e35e020aad
Michael Bestas <mikeioannina@gmail.com>
2024-03-06 13:23:18 +0200

    Temporarily remove fonts, icon pack, shape customization
    Revert "ThemePicker: Unify font/icon label sizes and scroll the text"
    
    This reverts commit 61bd0c04052cb167a407ebebefa48ff3a494a2ba.
    
    Change-Id: Id9923930206d7f6f10aa3d8a76774137a377d2dc
    
    Revert "ThemePicker: Vertically center the text of customization options"
    
    This reverts commit c83dc449b63100713094cb378daa47f648575e3a.
    
    Revert "ThemePicker: Bring back fonts, icon pack, shape customization"
    
    This reverts commit b5e469aef6ff64a3c69f9a597e4685a903ac510e.
    
    Change-Id: I93f9cdb9971eb743258ea8a0d1e65ff3b838b3a6

--- src/com/android/customization/model/ResourceConstants.java
+++ src/com/android/customization/model/ResourceConstants.java
@@ -108,6 +108,8 @@ public interface ResourceConstants {
         if (sTargetPackages.isEmpty()) {
             sTargetPackages.addAll(Arrays.asList(ANDROID_PACKAGE, SETTINGS_PACKAGE,
                     SYSUI_PACKAGE));
+            sTargetPackages.add(getLauncherPackage(context));
+            sTargetPackages.add(context.getPackageName());
         }
         return sTargetPackages.toArray(new String[0]);
     }
