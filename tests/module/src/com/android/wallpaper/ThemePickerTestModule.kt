9f486276ae55a6df405ed4ea2b9c1f8a1535cde3
Bhavuk Jain <bhavukj@google.com>
2024-05-16 19:54:25 +0000

    Injected FakeRequester
    This CL aims at injecting fakerequester in themepicker.
    
    Bug: 340970043
    Test: Tested by building & installing picker on local, testing via unit
    tests
    Flag: EXEMPT refactor
    
    Change-Id: Ifcded0fc0b8006c6b3d948ebce8aef47e12c9db9

--- tests/module/src/com/android/wallpaper/ThemePickerTestModule.kt
+++ tests/module/src/com/android/wallpaper/ThemePickerTestModule.kt
@@ -32,11 +32,13 @@ import com.android.wallpaper.module.WallpaperPreferences
 import com.android.wallpaper.module.logging.TestUserEventLogger
 import com.android.wallpaper.module.logging.UserEventLogger
 import com.android.wallpaper.modules.ThemePickerAppModule
+import com.android.wallpaper.network.Requester
 import com.android.wallpaper.picker.di.modules.EffectsModule
 import com.android.wallpaper.picker.preview.data.util.DefaultLiveWallpaperDownloader
 import com.android.wallpaper.picker.preview.data.util.LiveWallpaperDownloader
 import com.android.wallpaper.picker.preview.ui.util.DefaultImageEffectDialogUtil
 import com.android.wallpaper.picker.preview.ui.util.ImageEffectDialogUtil
+import com.android.wallpaper.testing.FakeDefaultRequester
 import com.android.wallpaper.testing.TestPartnerProvider
 import com.android.wallpaper.util.converter.DefaultWallpaperModelFactory
 import com.android.wallpaper.util.converter.WallpaperModelFactory
@@ -58,6 +60,8 @@ abstract class ThemePickerTestModule {
     @Binds @Singleton abstract fun bindInjector(impl: TestCustomizationInjector): Injector
 
     @Binds @Singleton abstract fun bindUserEventLogger(impl: TestUserEventLogger): UserEventLogger
+
+    @Binds @Singleton abstract fun bindFakeRequester(impl: FakeDefaultRequester): Requester
 
     @Binds
     @Singleton
