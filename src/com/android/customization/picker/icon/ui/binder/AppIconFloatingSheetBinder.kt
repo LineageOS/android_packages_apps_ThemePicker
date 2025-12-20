62fc8a1d1e0eefec846ecac622b4503e73c548f2
Michael Bestas <mkbestas@lineageos.org>
2025-12-20 17:23:14 +0200

    ThemePicker: Remove Beta tag from themed icons toggle
    Change-Id: Ibdc7f3081ceaea1528b74bf8685226b915d775f2

--- src/com/android/customization/picker/icon/ui/binder/AppIconFloatingSheetBinder.kt
+++ src/com/android/customization/picker/icon/ui/binder/AppIconFloatingSheetBinder.kt
@@ -17,7 +17,6 @@
 package com.android.customization.picker.icon.ui.binder
 
 import android.content.Context
-import android.content.res.ColorStateList
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
@@ -145,7 +144,6 @@ object AppIconFloatingSheetBinder {
         val themedIconsSwitch = view.requireViewById<MaterialSwitch>(R.id.themed_icon_toggle)
         val themedIconEntry = view.requireViewById<ViewGroup>(R.id.themed_icon_toggle_entry)
         val themedIconTitle = view.requireViewById<TextView>(R.id.themed_icon_toggle_title)
-        val themedIconBetaLabel = view.requireViewById<TextView>(R.id.themed_icon_beta_title)
         val showAppLabelsTitle = view.requireViewById<TextView>(R.id.show_app_labels_title)
         val showAppLabelsSwitch = view.requireViewById<MaterialSwitch>(R.id.show_app_labels_switch)
         ColorUpdateBinder.bind(
@@ -365,7 +363,6 @@ object AppIconFloatingSheetBinder {
                             titleBinding =
                                 bindTitleColor(
                                     themedIconTitle,
-                                    themedIconBetaLabel,
                                     colorUpdateViewModel,
                                     isFloatingSheetActive,
                                     lifecycleOwner,
@@ -396,7 +393,6 @@ object AppIconFloatingSheetBinder {
 
     private fun bindTitleColor(
         title: TextView,
-        betaLabel: TextView,
         colorUpdateViewModel: ColorUpdateViewModel,
         shouldAnimateColor: () -> Boolean,
         lifecycleOwner: LifecycleOwner,
@@ -408,27 +404,9 @@ object AppIconFloatingSheetBinder {
                 shouldAnimate = shouldAnimateColor,
                 lifecycleOwner = lifecycleOwner,
             )
-        val labelBinding =
-            ColorUpdateBinder.bind(
-                setColor = { color -> betaLabel.setTextColor(color) },
-                color = colorUpdateViewModel.colorOnPrimaryContainer,
-                shouldAnimate = shouldAnimateColor,
-                lifecycleOwner = lifecycleOwner,
-            )
-        val labelBackgroundBinding =
-            ColorUpdateBinder.bind(
-                setColor = { color ->
-                    betaLabel.background.setTintList(ColorStateList.valueOf(color))
-                },
-                color = colorUpdateViewModel.colorPrimaryContainer,
-                shouldAnimate = shouldAnimateColor,
-                lifecycleOwner = lifecycleOwner,
-            )
         return object : ColorUpdateBinder.Binding {
             override fun destroy() {
                 titleBinding.destroy()
-                labelBinding.destroy()
-                labelBackgroundBinding.destroy()
             }
         }
     }
